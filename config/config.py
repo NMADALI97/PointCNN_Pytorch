facb3a96ccc0c0a80b4e2f28f1aa1874800e02bd
MADALI NABIL <58610901+NMADALI97@users.noreply.github.com>
2020-12-26 22:36:26 +0100

    Update config.py

--- config/config.py
+++ config/config.py
@@ -3,11 +3,11 @@ import time
 import math
 
 
-base_model='modelnet_x3_l4'
+base_model='cifar10_x3_l4'
 
 
 # Note: dataset to use is determined by base_model
-dataset_available = ["ModelNet40","ScanNetSeg","ShapeNetParts"]
+dataset_available = ["ModelNet40","ScanNetSeg","ShapeNetParts","Cifar10"]
 dataset_to_path = {
     "ModelNet40": {
         "train": "/home/jxq/repository/ModelNet40/train_files.txt",
@@ -20,18 +20,24 @@ dataset_to_path = {
     "ShapeNetParts":{
         "train": "/home/jxq/repository/ModelNet40/train_files.txt",
         "test": "/home/jxq/repository/ModelNet40/test_files.txt"
+    },
+    "Cifar10":{
+        "train": "/home/jxq/repository/ModelNet40/train_files.txt",
+        "test": "/home/jxq/repository/ModelNet40/test_files.txt"
     }
 }
 
-base_model_available=['modelnet_x3_l4','scannet_x8_2048_k8_fps','shapenet_x8_2048_fps']
+base_model_available=['modelnet_x3_l4','scannet_x8_2048_k8_fps','shapenet_x8_2048_fps','cifar10_x3_l4']
 
 base_model_to_dataset={
     "modelnet_x3_l4":"ModelNet40",
-    "shapenet_x8_2048_fps":"ShapeNetParts"
+    "shapenet_x8_2048_fps":"ShapeNetParts",
+    "cifar10_x3_l4":"Cifar10"
 }
 
 base_model_to_task={
     "modelnet_x3_l4": "cls",
+    "cifar10_x3_l4": "cls",
     "shapenet_x8_2048_fps": "seg"
 }
 
@@ -53,6 +59,21 @@ base_model_to_dataset_setting={
         "jitter_val": 0.0,
         "rotation_range_val": [0, 0, 0, 'u'],
         "scaling_range_val": [0, 0, 0, 'u']
+    },
+    "cifar10_x3_l4":{
+        "sample_num":512,
+        "data_dim":6,
+        "use_extra_features" : True,
+        "with_X_transformation" : True,
+        "with_normal_feature":False,
+        "sorting_method" : None,
+        "rotation_range" : [0, 0, [0, math.pi], 'g'],
+        "rotation_order":'rzyx',
+        "scaling_range" : [0.0, [0.01], 0.0, 'u'],
+        "jitter":0,
+        "jitter_val": 0.0,
+        "rotation_range_val": [0, 0, 0, 'u'],
+        "scaling_range_val": [0, [0.01], 0, 'u']
     },
     "scannet_x8_2048_fps":{
         "sample_num":2048,
@@ -174,6 +195,19 @@ if base_model=="modelnet_x3_l4":
     validation.batch_size=128
     test.batch_size=128
     validation.step_val=5
+elif base_model=="cifar10_x3_l4":
+    train.batch_size=200
+    train.num_epochs=1024
+    train.optimizer="ADAM"
+    train.epsilon=1e-2
+    train.learning_rate_base=0.01
+    train.decay_steps=8000
+    train.decay_rate=0.5
+    train.learning_rate_min=1e-6
+    train.weight_decay=1e-5
+    validation.batch_size= 200
+    test.batch_size= 200
+    validation.step_val=500
 elif base_model=="shapenet_x8_2048_fps":
     train.batch_size=16
     train.num_epochs=1024
