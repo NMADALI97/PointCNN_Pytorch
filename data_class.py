3dde78109e15ff16887d8632805aeb03fe3d83c8
MADALI NABIL <58610901+NMADALI97@users.noreply.github.com>
2020-12-26 22:36:39 +0100

    Update data_class.py

--- data_class.py
+++ data_class.py
@@ -1,3 +1,17 @@
+
+
+
+
+
+import gzip
+import html
+import shutil
+import tarfile
+import zipfile
+import requests
+import argparse
+from tqdm import tqdm
+import random
 import os
 import h5py
 import numpy as np
@@ -10,45 +24,171 @@ import sys
 from datetime import datetime
 from dataset import pointfly as pf
 
-def load_data_s3dis(partition, point_num, data_dir='/mnt/dataset/s3dis/classification'):
-    h5_name = os.path.join(data_dir, partition + '_' + str(point_num) + '.hdf5')
-    all_data = []
-    all_label = []
-    assert os.path.isfile(h5_name), '{} does not exist.'.format(h5_name)
-    f = h5py.File(h5_name, 'r')
-    length = f['valid_count'][0]
-    data = f['data'][:length].astype('float32')
-    label = f['label'][:length].astype('int64')
-    f.close()
-    return data, label
-
-
-class S3DIS(Dataset):
-    def __init__(self, num_points=1024, partition='train', transforms = None):
-        #partition can be train or test
-        self.data, self.label = load_data_s3dis(partition, num_points)
-        self.num_points = num_points
-        self.partition = partition
-        self.transforms = transforms
-        #self.cat = get_catogrey()
-        #self.classes = list(self.cat.keys())
-     
-
-    def __getitem__(self, item):
-        pointcloud = self.data[item][:self.num_points,:3]
-        label = self.label[item]
-        label = np.expand_dims(label,axis=0)
-        if self.partition == 'train':
-            np.random.shuffle(pointcloud)
-        if self.transforms is not None:
-            pointcloud = self.transforms(pointcloud)
-        return pointcloud, label
-
-    def __len__(self):
-        return self.data.shape[0]
 
 
 
+
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        batch = pickle.load(fo, encoding='bytes')
+    return batch
+
+
+
+# from https://gist.github.com/hrouault/1358474
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+    The "answer" return value is one of "yes" or "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default == None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' (or 'y' or 'n').\n")
+
+
+def download_from_url(url, dst):
+    download = True
+    if os.path.exists(dst):
+        download = query_yes_no('Seems you have downloaded %s to %s, overwrite?' % (url, dst), default='no')
+        if download:
+            os.remove(dst)
+
+    if download:
+        response = requests.get(url, stream=True)
+        total_size = int(response.headers.get('content-length', 0))
+        chunk_size = 1024 * 1024
+        bars = total_size // chunk_size
+        with open(dst, "wb") as handle:
+            for data in tqdm(response.iter_content(chunk_size=chunk_size), total=bars, desc=url.split('/')[-1],
+                             unit='M'):
+                handle.write(data)
+
+
+def download_and_unzip(url, root, dataset):
+    folder = os.path.join(root, dataset)
+    folder_zips = os.path.join(folder, 'zips')
+    if not os.path.exists(folder_zips):
+        os.makedirs(folder_zips)
+    filename_zip = os.path.join(folder_zips, url.split('/')[-1])
+
+    download_from_url(url, filename_zip)
+
+    if filename_zip.endswith('.zip'):
+        zip_ref = zipfile.ZipFile(filename_zip, 'r')
+        zip_ref.extractall(folder)
+        zip_ref.close()
+    elif filename_zip.endswith(('.tar.gz', '.tgz')):
+        tarfile.open(name=filename_zip, mode="r:gz").extractall(folder)
+    elif filename_zip.endswith('.gz'):
+        filename_no_gz = filename_zip[:-3]
+        with gzip.open(filename_zip, 'rb') as f_in, open(filename_no_gz, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+
+
+def cifar10_download():
+    BASE_DIR = os.path.dirname(os.path.abspath("__file__"))
+    DATA_DIR = os.path.join(BASE_DIR, 'data')
+    if not os.path.exists(DATA_DIR):
+        os.mkdir(DATA_DIR)
+    if not os.path.exists(os.path.join(DATA_DIR, 'cifar10')):
+        download_and_unzip('https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz', "data", "cifar10")
+        batch_size = 2048
+
+        folder_cifar10 = 'data/cifar10/cifar-10-batches-py'
+        folder_pts = os.path.join(os.path.dirname(folder_cifar10), 'pts')
+
+        train_test_files = [('train', ['data_batch_%d' % (idx + 1) for idx in range(5)]),
+                                ('test', ['test_batch'])]
+
+        data = np.zeros((batch_size, 1024, 6))
+        label = np.zeros((batch_size), dtype=np.int32)
+        for tag, filelist in train_test_files: 
+                data_list = []
+                labels_list = []
+                for filename in filelist:
+                    batch = unpickle(os.path.join(folder_cifar10, filename))
+                    data_list.append(np.reshape(batch[b'data'], (10000, 3, 32, 32)))
+                    labels_list.append(batch[b'labels'])
+                images = np.concatenate(data_list, axis=0)
+                labels = np.concatenate(labels_list, axis=0)
+
+                idx_h5 = 0
+                filename_filelist_h5 = os.path.join(os.path.dirname(folder_cifar10), '%s_files.txt' % tag)
+                with open(filename_filelist_h5, 'w') as filelist_h5:
+                    for idx_img, image in enumerate(images):
+                        points = []
+                        pixels = []
+                        for x in range(32):
+                            for z in range(32):
+                                points.append((x, random.random() * 1e-6, z))
+                                pixels.append((image[0, x, z], image[1, x, z], image[2, x, z]))
+                        points_array = np.array(points)
+                        pixels_array = (np.array(pixels).astype(np.float32) / 255)-0.5
+
+                        points_min = np.amin(points_array, axis=0)
+                        points_max = np.amax(points_array, axis=0)
+                        points_center = (points_min + points_max) / 2
+                        scale = np.amax(points_max - points_min) / 2
+                        points_array = (points_array - points_center) * (0.8 / scale)
+
+                        
+
+                        idx_in_batch = idx_img % batch_size
+                        data[idx_in_batch, ...] = np.concatenate((points_array, pixels_array), axis=-1)
+                        label[idx_in_batch] = labels[idx_img]
+                        if ((idx_img + 1) % batch_size == 0) or idx_img == len(images) - 1:
+                            item_num = idx_in_batch + 1
+                            filename_h5 = os.path.join(os.path.dirname(folder_cifar10), '%s_%d.h5' % (tag, idx_h5))
+                            print('{}-Saving {}...'.format(datetime.now(), filename_h5))
+                            filelist_h5.write('./%s_%d.h5\n' % (tag, idx_h5))
+
+                            file = h5py.File(filename_h5, 'w')
+                            file.create_dataset('data', data=data[0:item_num, ...])
+                            file.create_dataset('label', data=label[0:item_num, ...])
+                            file.close()
+
+                            idx_h5 = idx_h5 + 1
+
+
+def cifar10_load_data(partition):   
+    cifar10_download()
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    DATA_DIR = os.path.join(BASE_DIR, 'data')
+    points = []
+    labels = []
+    if partition == 'trainval':
+        file = glob.glob(os.path.join(DATA_DIR, 'cifar10', '*train*.h5')) \
+               + glob.glob(os.path.join(DATA_DIR, 'cifar10', '*val*.h5'))
+    else:
+        file = glob.glob(os.path.join(DATA_DIR, 'cifar10', '*%s*.h5'%partition))
+    for h5_name in file:
+        data = h5py.File(h5_name, 'r+')
+        points.append(data['data'][...].astype(np.float32))
+        labels.append(data['label'][...].astype(np.int64))
+    return (np.concatenate(points, axis=0),np.concatenate(labels, axis=0))
+            
+        
 
 def modelnet40_download():
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -277,4 +417,15 @@ class ModelNet40(Dataset):
     def get_categories(self):
       return [ "airplane", "bathtub", "bed", "bench", "bookshelf", "bottle", "bowl", "car", "chair", "cone", "cup", "curtain", "desk", "door", "dresser", "flower_pot", "glass_box", "guitar", "keyboard", "lamp", "laptop", "mantel", "monitor", "night_stand", "person", "piano", "plant", "radio", "range_hood", "sink", "sofa", "stairs", "stool", "table", "tent", "toilet", "tv_stand", "vase", "wardrobe", "xbox", ]
 
+class Cifar10(Dataset):
+    def __init__(self, partition='train'):
+        self.data, self.label = cifar10_load_data(partition)
+        self.partition = partition
+    def __getitem__(self, item):
+        pointcloud = self.data[item]
+        label = self.label[item]
+        return pointcloud, label
+
+    def __len__(self):
+        return self.data.shape[0]
     
