14000c7184d766a0076465ebd698ec1a06c71720
MADALI NABIL <58610901+NMADALI97@users.noreply.github.com>
2021-01-13 11:43:14 +0100

    Update config.py

--- config/config.py
+++ config/config.py
@@ -7,9 +7,17 @@ base_model='s3dis_x8_2048_fps'
 
 
 # Note: dataset to use is determined by base_model
-dataset_available = ["ModelNet40","ScanNetSeg","ShapeNetParts","Cifar10","S3DIS"]
+dataset_available = ["ModelNet40","ModelNet10","Mnist","ScanNetSeg","ShapeNetParts","Cifar10","S3DIS"]
 dataset_to_path = {
     "ModelNet40": {
+        "train": "/home/jxq/repository/ModelNet40/train_files.txt",
+        "test": "/home/jxq/repository/ModelNet40/test_files.txt"
+    },
+     "ModelNet10": {
+        "train": "/home/jxq/repository/ModelNet40/train_files.txt",
+        "test": "/home/jxq/repository/ModelNet40/test_files.txt"
+    },
+     "Mnist": {
         "train": "/home/jxq/repository/ModelNet40/train_files.txt",
         "test": "/home/jxq/repository/ModelNet40/test_files.txt"
     },
@@ -31,10 +39,12 @@ dataset_to_path = {
     }
 }
 
-base_model_available=['modelnet_x3_l4','scannet_x8_2048_k8_fps','shapenet_x8_2048_fps','cifar10_x3_l4','s3dis_x8_2048_fps']
+base_model_available=['modelnet_x3_l4','modelnet_10_x3_l4',"mnist_x3_l4",'scannet_x8_2048_k8_fps','shapenet_x8_2048_fps','cifar10_x3_l4','s3dis_x8_2048_fps']
 
 base_model_to_dataset={
     "modelnet_x3_l4":"ModelNet40",
+    "modelnet_10_x3_l4":"ModelNet10",
+    "mnist_x3_l4":"Mnist",
     "shapenet_x8_2048_fps":"ShapeNetParts",
     "cifar10_x3_l4":"Cifar10",
     "s3dis_x8_2048_fps":"S3DIS"
@@ -42,6 +52,8 @@ base_model_to_dataset={
 
 base_model_to_task={
     "modelnet_x3_l4": "cls",
+    "modelnet_10_x3_l4": "cls",
+    "mnist_x3_l4": "cls",
     "cifar10_x3_l4": "cls",
     "shapenet_x8_2048_fps": "seg",
     "s3dis_x8_2048_fps": "seg"
@@ -55,6 +67,36 @@ base_model_to_dataset_setting={
         "sample_num":1024,
         "data_dim":6,
         "use_extra_features" : False,
+        "with_X_transformation" : True,
+        "with_normal_feature":True,
+        "sorting_method" : None,
+        "rotation_range" : [0,math.pi,0,'u'],
+        "rotation_order":'rxyz',
+        "scaling_range" : [0.1, 0.1, 0.1, 'g'],
+        "jitter":0,
+        "jitter_val": 0.0,
+        "rotation_range_val": [0, 0, 0, 'u'],
+        "scaling_range_val": [0, 0, 0, 'u']
+    },
+    "modelnet_10_x3_l4":{
+        "sample_num":1024,
+        "data_dim":3,
+        "use_extra_features" : False,
+        "with_X_transformation" : True,
+        "with_normal_feature":False,
+        "sorting_method" : None,
+        "rotation_range" : [0,math.pi,0,'u'],
+        "rotation_order":'rxyz',
+        "scaling_range" : [0.1, 0.1, 0.1, 'g'],
+        "jitter":0,
+        "jitter_val": 0.0,
+        "rotation_range_val": [0, 0, 0, 'u'],
+        "scaling_range_val": [0, 0, 0, 'u']
+    },
+    "mnist_x3_l4":{
+        "sample_num":2048,
+        "data_dim":6,
+        "use_extra_features" : True,
         "with_X_transformation" : True,
         "with_normal_feature":True,
         "sorting_method" : None,
@@ -109,34 +151,37 @@ base_model_to_dataset_setting={
     },
      "s3dis_x8_2048_fps":{
         "sample_num": 2048,
-        "data_dim": 6,
+        "data_dim": 9,
         "use_extra_features" : True,
-        "with_normal_feature":False,
+        "with_normal_feature": True,
         "with_X_transformation": True,
         "sorting_method": None,
-        "rotation_range": [0, math.pi/32., 0, 'u'],
+        "rotation_range": [0, 0, 0, 'u'],
         "rotation_order": 'rxyz',
-        "scaling_range":  [0.001, 0.001, 0.001, 'g'],
-        "jitter": 0.0,
+        "scaling_range": [0.0, 0.0, 0.0, 'g'],
+        "jitter": 0.001,
         "jitter_val": 0.0,
         "rotation_range_val": [0, 0, 0, 'u'],
         "scaling_range_val": [0, 0, 0, 'u']
     }
 }
-assert dataset in dataset_available
+
 dataset_dir = dataset_to_path[dataset]
 dataset_setting=base_model_to_dataset_setting[base_model]
 
 
 # configuration file
-project_name = "PyG_PointCNN"
+project_name = "PointCNN"
 description = ""
 
 
 # device can be either "cuda" or "cpu"
 num_workers = 4
-available_gpus = "1"
-if available_gpus!="":
+
+
+
+available_gpus = str( torch.cuda.device_count())
+if  torch.cuda.is_available():
     device = torch.device("cuda")
     use_gpu=True
 else:
@@ -163,7 +208,7 @@ class train:
     root_dir = dataset_to_path[dataset]
     if isinstance(root_dir, dict):
         root_dir = root_dir['train']
-    resume = False
+    resume =False
     resume_epoch = None
 
 ################## DO NOT CHANGE ############################
@@ -185,7 +230,7 @@ if process != 'TRAIN':
 result_root = "rst-{}".format(project_name)
 result_sub_folder = "{}/{}".format(result_root, comment)
 ckpt_file = "{}/ckpt.pth".format(result_sub_folder)
-
+resume_ckpt_file="rst-PointCNN/shapenet_x8_2048_fps_210111_101050/ckpt.pth"
 
 #############################################################
 
@@ -216,6 +261,32 @@ if base_model=="modelnet_x3_l4":
     validation.batch_size=128
     test.batch_size=128
     validation.step_val=5
+elif base_model=="mnist_x3_l4":
+    train.batch_size=128
+    train.num_epochs=10
+    train.optimizer="ADAM"
+    train.epsilon=1e-2
+    train.learning_rate_base=0.01
+    train.decay_steps=8000
+    train.decay_rate=0.5
+    train.learning_rate_min=1e-6
+    train.weight_decay=1e-5
+    validation.batch_size=128
+    test.batch_size=128
+    validation.step_val=5
+elif base_model=="modelnet_10_x3_l4":
+    train.batch_size=32
+    train.num_epochs=200
+    train.optimizer="ADAM"
+    train.epsilon=1e-2
+    train.learning_rate_base=0.01
+    train.decay_steps=8000
+    train.decay_rate=0.5
+    train.learning_rate_min=1e-6
+    train.weight_decay=1e-5
+    validation.batch_size=32
+    test.batch_size=32
+    validation.step_val=5
 elif base_model=="cifar10_x3_l4":
     train.batch_size=200
     train.num_epochs=1024
@@ -231,7 +302,7 @@ elif base_model=="cifar10_x3_l4":
     validation.step_val=500
 elif base_model=="shapenet_x8_2048_fps":
     train.batch_size=16
-    train.num_epochs=1024
+    train.num_epochs=10
     train.optimizer="ADAM"
     train.epsilon=1e-3
     train.learning_rate_base=0.005
@@ -239,9 +310,9 @@ elif base_model=="shapenet_x8_2048_fps":
     train.decay_rate= 0.9
     train.learning_rate_min=0.00001
     train.weight_decay=0.0
-    validation.batch_size=16
+    validation.batch_size=32
     validation.step_val=5
-    test.batch_size=16
+    test.batch_size=32
 elif base_model=="s3dis_x8_2048_fps":
     train.batch_size=16
     train.num_epochs=1024
