07e4c3bed4f8f58b382bd5c67c7fbbb15dc064aa
MADALI NABIL <58610901+NMADALI97@users.noreply.github.com>
2021-01-13 11:43:19 +0100

    Update data_class.py

--- data_class.py
+++ data_class.py
@@ -20,13 +20,64 @@ from torch.utils.data import Dataset
 import json
 from plyfile import PlyData, PlyElement
 import torch
+from tqdm import tqdm
 import sys
 from datetime import datetime
 from dataset import pointfly as pf
-
+import scipy.spatial.distance
 from sklearn.model_selection import train_test_split
+from transforms_3d import *
+def read_off(file):
+    if 'OFF' != file.readline().strip():
+        raise('Not a valid OFF header')
+    n_verts, n_faces, __ = tuple([int(s) for s in file.readline().strip().split(' ')])
+    verts = [[float(s) for s in file.readline().strip().split(' ')] for i_vert in range(n_verts)]
+    faces = [[int(s) for s in file.readline().strip().split(' ')][1:] for i_face in range(n_faces)]
+    return verts, faces
 
+class PointSampler(object):
+    def __init__(self, output_size):
+        assert isinstance(output_size, int)
+        self.output_size = output_size
+    
+    def triangle_area(self, pt1, pt2, pt3):
+        side_a = np.linalg.norm(pt1 - pt2)
+        side_b = np.linalg.norm(pt2 - pt3)
+        side_c = np.linalg.norm(pt3 - pt1)
+        s = 0.5 * ( side_a + side_b + side_c)
+        return max(s * (s - side_a) * (s - side_b) * (s - side_c), 0)**0.5
 
+    def sample_point(self, pt1, pt2, pt3):
+        # barycentric coordinates on a triangle
+        # https://mathworld.wolfram.com/BarycentricCoordinates.html
+        s, t = sorted([random.random(), random.random()])
+        f = lambda i: s * pt1[i] + (t-s)*pt2[i] + (1-t)*pt3[i]
+        return (f(0), f(1), f(2))
+        
+    
+    def __call__(self, mesh):
+        verts, faces = mesh
+        verts = np.array(verts)
+        areas = np.zeros((len(faces)))
+
+        for i in range(len(areas)):
+            areas[i] = (self.triangle_area(verts[faces[i][0]],
+                                           verts[faces[i][1]],
+                                           verts[faces[i][2]]))
+            
+        sampled_faces = (random.choices(faces, 
+                                      weights=areas,
+                                      cum_weights=None,
+                                      k=self.output_size))
+        
+        sampled_points = np.zeros((self.output_size, 3))
+
+        for i in range(len(sampled_faces)):
+            sampled_points[i] = (self.sample_point(verts[sampled_faces[i][0]],
+                                                   verts[sampled_faces[i][1]],
+                                                   verts[sampled_faces[i][2]]))
+        
+        return sampled_points
 
 
 def unpickle(file):
@@ -172,22 +223,7 @@ def cifar10_download():
                             idx_h5 = idx_h5 + 1
 
 
-def cifar10_load_data(partition):   
-    cifar10_download()
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    DATA_DIR = os.path.join(BASE_DIR, 'data')
-    points = []
-    labels = []
-    if partition == 'trainval':
-        file = glob.glob(os.path.join(DATA_DIR, 'cifar10', '*train*.h5')) \
-               + glob.glob(os.path.join(DATA_DIR, 'cifar10', '*val*.h5'))
-    else:
-        file = glob.glob(os.path.join(DATA_DIR, 'cifar10', '*%s*.h5'%partition))
-    for h5_name in file:
-        data = h5py.File(h5_name, 'r+')
-        points.append(data['data'][...].astype(np.float32))
-        labels.append(data['label'][...].astype(np.int64))
-    return (np.concatenate(points, axis=0),np.concatenate(labels, axis=0))
+"""
             
 def s3dis_download():       
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -218,7 +254,28 @@ def s3dis_download():
             for path in X_test:
                 filelist.write(path+"\n")
                           
+"""
 
+def s3dis_download():       
+    BASE_DIR = os.path.dirname(os.path.abspath("__file__"))
+    DATA_DIR = os.path.join(BASE_DIR, 'data')
+    if not os.path.exists(DATA_DIR):
+        os.mkdir(DATA_DIR)
+    if not os.path.exists(os.path.join(DATA_DIR, 'indoor3d_sem_seg_hdf5_data')):
+        
+        www = 'https://drive.google.com/uc?id=1VQwPXMdMbq6MF0yonEeJeDf7A6RdfH2a'
+        zipfile = "indoor3d_sem_seg_hdf5_data.zip"
+        os.system('gdown  %s; unzip %s -d data' % (www, zipfile))
+        os.system('rm %s' % (zipfile))
+        X_train, X_test = train_test_split(glob.glob(os.path.join("data/indoor3d_sem_seg_hdf5_data", '*.h5')) , test_size=0.15) 
+        with open("data/indoor3d_sem_seg_hdf5_data/train_file.txt", 'w') as filelist:
+            for path in X_train:
+                filelist.write(path+"\n")
+
+
+        with open("data/indoor3d_sem_seg_hdf5_data/valid_file.txt", 'w') as filelist:
+            for path in X_test:
+                filelist.write(path+"\n")
 def modelnet40_download():
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     #BASE_DIR = os.getcwd()
@@ -231,7 +288,43 @@ def modelnet40_download():
         os.system('wget  --no-check-certificate %s; unzip %s' % (www, zipfile))
         os.system('mv %s %s' % (zipfile[:-4], DATA_DIR))
         os.system('rm %s' % (zipfile))
-
+def modelnet10_download():
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    #BASE_DIR = os.getcwd()
+    DATA_DIR = os.path.join(BASE_DIR, 'data')
+    if not os.path.exists(DATA_DIR):
+        os.mkdir(DATA_DIR)
+    if not os.path.exists(os.path.join(DATA_DIR, 'ModelNet10')):
+        www = 'http://3dvision.princeton.edu/projects/2014/3DShapeNets/ModelNet10.zip'
+        zipfile = os.path.basename(www)
+        os.system('wget  --no-check-certificate %s; unzip %s -d data' % (www, zipfile))
+        os.system('rm %s' % (zipfile))
+def mnist_download():
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    #BASE_DIR = os.getcwd()
+    DATA_DIR = os.path.join(BASE_DIR, 'data')
+    if not os.path.exists(DATA_DIR):
+        os.mkdir(DATA_DIR)
+    
+    if not os.path.exists(os.path.join(DATA_DIR, 'Mnist')):
+        if not os.path.exists(os.path.join(DATA_DIR, 'Mnist')):
+          os.mkdir(os.path.join(DATA_DIR, 'Mnist'))
+        www = 'https://drive.google.com/uc?id=1eYAKTDBgQhFngsZ3Uu9_0nGOxqdMYJm0'
+        zipfile = "archive.zip"
+        os.system('gdown %s; unzip %s -d data/Mnist' % (www, zipfile))
+        os.system('rm %s' % (zipfile))
+def download_shapenetpart():
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    #BASE_DIR = os.getcwd()
+    DATA_DIR = os.path.join(BASE_DIR, 'data')
+    if not os.path.exists(DATA_DIR):
+        os.mkdir(DATA_DIR)
+    if not os.path.exists(os.path.join(DATA_DIR, 'hdf5_data')):
+        www = 'https://shapenet.cs.stanford.edu/media/shapenet_part_seg_hdf5_data.zip'
+        zipfile = os.path.basename(www)
+        os.system('wget  --no-check-certificate %s; unzip %s -d data' % (www, zipfile))
+        os.system('rm %s' % (zipfile))
+"""
 def download_shapenetpart():
     BASE_DIR = os.path.dirname(os.path.abspath("__file__"))
     DATA_DIR = os.path.join(BASE_DIR, 'data')
@@ -354,7 +447,23 @@ def download_shapenetpart():
                     for part in ("train", "val"):
                         part_txt = os.path.join(root, "%s_files.txt" % part)
                         train_val.write(open(part_txt, "r").read())
-
+"""
+def cifar10_load_data(partition):   
+    cifar10_download()
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    DATA_DIR = os.path.join(BASE_DIR, 'data')
+    points = []
+    labels = []
+    if partition == 'trainval':
+        file = glob.glob(os.path.join(DATA_DIR, 'cifar10', '*train*.h5')) \
+               + glob.glob(os.path.join(DATA_DIR, 'cifar10', '*val*.h5'))
+    else:
+        file = glob.glob(os.path.join(DATA_DIR, 'cifar10', '*%s*.h5'%partition))
+    for h5_name in file:
+        data = h5py.File(h5_name, 'r+')
+        points.append(data['data'][...].astype(np.float32))
+        labels.append(data['label'][...].astype(np.int64))
+    return (np.concatenate(points, axis=0),np.concatenate(labels, axis=0))
 def load_data_partseg(partition):
     download_shapenetpart()
     BASE_DIR = os.path.dirname(os.path.abspath("__file__"))
@@ -365,26 +474,131 @@ def load_data_partseg(partition):
     labels_seg = []
     indices_split_to_full = []
     if partition == 'trainval':
-        file = glob.glob(os.path.join(DATA_DIR, 'shapenet*hdf5*', '*train*.h5')) \
-               + glob.glob(os.path.join(DATA_DIR, 'shapenet*hdf5*', '*val*.h5'))
+        file = glob.glob(os.path.join(DATA_DIR, 'hdf5_data', '*train*.h5')) \
+               + glob.glob(os.path.join(DATA_DIR, 'hdf5_data', '*val*.h5'))
     else:
-        file = glob.glob(os.path.join(DATA_DIR, 'shapenet*hdf5*', '*%s*.h5'%partition))
+        file = glob.glob(os.path.join(DATA_DIR, 'hdf5_data', '*%s*.h5'%partition))
     for h5_name in file:
         data = h5py.File(h5_name, 'r+')
         points.append(data['data'][...].astype(np.float32))
         labels.append(data['label'][...].astype(np.int64))
-        point_nums.append(data['data_num'][...].astype(np.int32))
-        labels_seg.append(data['label_seg'][...].astype(np.int64))
+        for u in  data['data'][...].astype(np.int64):
+            point_nums.append(u.shape[0])
+        labels_seg.append(data['pid'][...].astype(np.int64))
         if 'indices_split_to_full' in data:
             indices_split_to_full.append(data['indices_split_to_full'][...].astype(np.int64))
-    
+
     return (np.concatenate(points, axis=0),
             np.concatenate(labels, axis=0),
-            np.concatenate(point_nums, axis=0),
+            np.array(point_nums).reshape(-1),
             np.concatenate(labels_seg, axis=0),
             np.concatenate(indices_split_to_full, axis=0) if indices_split_to_full else None)
+def load_data(partition):
+    modelnet40_download()
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    #BASE_DIR = os.getcwd()
+    DATA_DIR = os.path.join(BASE_DIR, 'modelnet40_data')
+    all_data = []
+    all_label = []
+    for h5_name in glob.glob(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048', 'ply_data_%s*.h5'%partition)):
+        f = h5py.File(h5_name)
+        data = f['data'][:].astype('float32')
+        label = f['label'][:].astype('int64')
+        f.close()
+        all_data.append(data)
+        all_label.append(label)
+    all_data = np.concatenate(all_data, axis=0)
+    all_label = np.concatenate(all_label, axis=0)
+    return all_data, all_label
+def modelnet10_load_data(partition):
+    modelnet10_download()
+    all_data = []
+    all_label = []
+    root_dir = "data/ModelNet10"
+    folders = [dir for dir in sorted(os.listdir(root_dir)) if os.path.isdir(os.path.join(root_dir,dir) )]
+    classes = {folder: i for i, folder in enumerate(folders)}
+    files = []
+    for category in classes.keys():
+            new_dir =os.path.join(root_dir,category,partition) 
+            for file in os.listdir(new_dir):
+                if file.endswith('.off'):
+                    sample = {}
+                    sample['pcd_path'] = os.path.join(new_dir,file)  
+                    sample['category'] = category
+                    files.append(sample)
+
+        
+    for idx in  tqdm(range(len(files))):
+        pcd_path = files[idx]['pcd_path']
+        category = files[idx]['category']
+        with open(pcd_path, 'r') as f:
+            verts, faces = read_off(f)
+            pointcloud =PointSampler(1024)((verts, faces))
+            
+        data,label= pointcloud.astype('float32'), classes[category]
+
+        all_data.append(data)
+        all_label.append(label)
+
+    return np.array(all_data,dtype=np.float32),np.array(all_label,dtype=np.int64)
+def mnist_load_data(partition):
+    mnist_download()
+    all_data = []
+    all_label = []
+    root_dir = "data/Mnist"
+    if partition=="train":
+        path=os.path.join(root_dir,"train_point_clouds.h5")
+    else:
+     path=os.path.join(root_dir,"test_point_clouds.h5")
+    hf = h5py.File(path,'r+') 
+    for idx in list(hf.keys()):
+        a = hf[idx]
+        pc=np.concatenate([a["points"][:], a['normals'][:]], axis=1)
+
+        choices = np.random.choice(pc.shape[0], 2048, replace=True)
+        
+        all_data.append(pc[choices])
+        all_label.append(a.attrs["label"])
 
 
+    return np.array(all_data,dtype=np.float32),np.array(all_label,dtype=np.int64)  
+
+def load_data_s3dis(partition):
+    s3dis_download()
+    BASE_DIR = os.path.dirname(os.path.abspath("__file__"))
+    DATA_DIR = os.path.join(BASE_DIR, 'data')
+    points = []
+    labels = []
+    point_nums = []
+    labels_seg = []
+    indices_split_to_full = []
+    if partition == 'train':
+        filelist="data/indoor3d_sem_seg_hdf5_data/train_file.txt"
+    else:
+        filelist="data/indoor3d_sem_seg_hdf5_data/valid_file.txt"
+    file=[line.strip() for line in open(filelist)]
+    
+    for h5_name in file:
+        data = h5py.File(h5_name, 'r+')
+        xyz=data['data'][...].astype(np.float32)
+        fts=data['label'][...].astype(np.int64)
+        tmp1=[]
+        tmp2=[]
+        for i in range(xyz.shape[0]):
+            pc=xyz[i]
+            choices = np.random.choice(pc.shape[0], 2048, replace=False)
+
+            tmp1.append(pc[choices,:])
+            tmp2.append((fts[i])[choices])
+        points.append(np.array(tmp1,dtype=np.float32).reshape((xyz.shape[0],2048,9)))
+        labels_seg.append(np.array(tmp2,dtype=np.int64).reshape((xyz.shape[0],2048)))
+
+        for u in  data['data'][...].astype(np.int64):
+            point_nums.append(2048)
+        
+
+    return (np.concatenate(points, axis=0), None, np.array(point_nums).reshape(-1), np.concatenate(labels_seg, axis=0), None)
+"""
 def load_data_s3dis(partition):
     s3dis_download()
     BASE_DIR = os.path.dirname(os.path.abspath("__file__"))
@@ -413,15 +627,17 @@ def load_data_s3dis(partition):
             np.concatenate(point_nums, axis=0),
             np.concatenate(labels_seg, axis=0),
             np.concatenate(indices_split_to_full, axis=0) if indices_split_to_full else None)
+"""
 class ShapeNetPart(Dataset):
-    def __init__(self, partition='trainval'):
+    def __init__(self, partition='trainval',transforms=None):
         super().__init__()
         #self.batch_size = batch_size
         #self.shuffle = shuffle
        
         self.points, self.labels, self.point_nums, self.labels_seg, _  = load_data_partseg(partition)
-        self.points=torch.Tensor(pf.global_norm(self.points))
-      
+        
+        self.points=pf.global_norm(self.points)
+        
 
         print(self.points.shape)
         print(self.labels_seg.shape)
@@ -429,19 +645,23 @@ class ShapeNetPart(Dataset):
       
 
     def __getitem__(self, index):
+        
         return self.points[index], self.labels_seg[index], self.point_nums[index],self.labels[index]
 
     def __len__(self):
         return self.points.shape[0]
+
+
 class S3DIS(Dataset):
-    def __init__(self, partition='train'):
+    def __init__(self, partition='train',transforms=None):
         super().__init__()
         #self.batch_size = batch_size
         #self.shuffle = shuffle
        
-        self.points, self.labels, self.point_nums, self.labels_seg, _  = load_data_s3dis(partition)
-        self.points=torch.Tensor(pf.global_norm(self.points))
+        self.points, _, self.point_nums, self.labels_seg, _  = load_data_s3dis(partition)
       
+        self.points=pf.global_norm(self.points)
+        
 
         print(self.points.shape)
         print(self.labels_seg.shape)
@@ -449,42 +669,30 @@ class S3DIS(Dataset):
       
 
     def __getitem__(self, index):
-        return self.points[index], self.labels_seg[index], self.point_nums[index],self.labels[index]
+        
+        return self.points[index], self.labels_seg[index], self.point_nums[index],-1
+
+        
 
     def __len__(self):
         return self.points.shape[0]
 
-def load_data(partition):
-    modelnet40_download()
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    #BASE_DIR = os.getcwd()
-    DATA_DIR = os.path.join(BASE_DIR, 'modelnet40_data')
-    all_data = []
-    all_label = []
-    for h5_name in glob.glob(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048', 'ply_data_%s*.h5'%partition)):
-        f = h5py.File(h5_name)
-        data = f['data'][:].astype('float32')
-        label = f['label'][:].astype('int64')
-        f.close()
-        all_data.append(data)
-        all_label.append(label)
-    all_data = np.concatenate(all_data, axis=0)
-    all_label = np.concatenate(all_label, axis=0)
-    return all_data, all_label
+
+
 
 
 class ModelNet40(Dataset):
-    def __init__(self, partition='train'):
+    def __init__(self, partition='train',transforms=None):
         self.data, self.label = load_data(partition)
         #self.num_points = num_points
         self.partition = partition
-       # self.transforms = transforms
+        self.transforms = transforms
         #self.cat = get_catogrey()
         #self.classes = list(self.cat.keys())
      
 
     def __getitem__(self, item):
-        pointcloud = self.data[item]
+        pointcloud = self.transforms(self.data[item])
         label = self.label[item]
         return pointcloud, label
 
@@ -504,4 +712,71 @@ class Cifar10(Dataset):
 
     def __len__(self):
         return self.data.shape[0]
-    
+
+
+class Mnist(Dataset):
+    def __init__(self, partition='train'):
+        self.data, self.label =mnist_load_data(partition)
+        self.partition = partition
+      
+     
+
+    def __getitem__(self, item):
+        pointcloud = self.data[item]
+        label = self.label[item]
+        return pointcloud, label
+
+    def __len__(self):
+        return self.data.shape[0]
+    def get_categories(self):
+      return [str(i) for i in range(10)]
+        
+
+class ModelNet10(Dataset):
+    def __init__(self, partition='train'):
+        self.data, self.label = modelnet10_load_data(partition)
+        self.partition = partition
+      
+     
+
+    def __getitem__(self, item):
+        pointcloud = self.data[item]
+        label = self.label[item]
+        return pointcloud, label
+
+    def __len__(self):
+        return self.data.shape[0]
+    def get_categories(self):
+      return [ 'bathtub', 'bed' , 'chair', 'desk', 'dresser', 'monitor', 'night_stand', 'sofa', 'table', 'toilet' ]
+"""
+class ModelNet10(Dataset):
+    def __init__(self, root_dir="data/ModelNet10", partition="train"):
+        modelnet10_download()
+        self.root_dir = root_dir
+        folders = [dir for dir in sorted(os.listdir(root_dir)) if os.path.isdir(os.path.join(root_dir,dir) )]
+        self.classes = {folder: i for i, folder in enumerate(folders)}
+        self.files = []
+        for category in self.classes.keys():
+            new_dir =os.path.join(root_dir,category,partition) 
+            for file in os.listdir(new_dir):
+                if file.endswith('.off'):
+                    sample = {}
+                    sample['pcd_path'] = os.path.join(new_dir,file)  
+                    sample['category'] = category
+                    self.files.append(sample)
+
+        
+    def __len__(self):
+        return len(self.files)
+
+    def __preproc__(self, file):
+        verts, faces = read_off(file)
+        return PointSampler(1024)((verts, faces))
+
+    def __getitem__(self, idx):
+        pcd_path = self.files[idx]['pcd_path']
+        category = self.files[idx]['category']
+        with open(pcd_path, 'r') as f:
+            pointcloud = self.__preproc__(f)
+        return pointcloud.astype('float32'), (self.classes[category])
+"""
